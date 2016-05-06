f3115ba9c57cf44326118f126f1f18e9fd3dc60e
Rub21 <ruben@mapbox.com>
2016-05-06 16:52:55 -0500

    crossingWaterwaysHighways

---
+++ converters/crossingWaterwaysHighways/Readme.md
@@ -0,0 +1,5 @@
+## Execute 
+
+```
+osmlint2csv --conv=crossinghighways --type=major-major,major-minor crossinghighways.tofix.json > crossing-highways-major.csv
+```
---
+++ converters/crossingWaterwaysHighways/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var fs = require('fs');
+var readline = require('readline');
+
+module.exports = function(inputFile, type, done) {
+  var types = type.split(',');
+  var rd = readline.createInterface({
+    input: fs.createReadStream(inputFile),
+    output: process.stdout,
+    terminal: false
+  });
+  var header = 'way,geom';
+  //Print CSV header
+  console.log(header);
+  rd.on('line', function(line) {
+    var obj = JSON.parse(line);
+    var features = obj.features;
+    var coordinates = {};
+    for (var i = 0; i < features.length; i++) {
+      var val = features[i];
+      if (coordinates[val.geometry.coordinates.join(',')] === undefined) {
+        if (val.geometry.type === 'Point' && types.indexOf(val.properties._type) > -1) {
+          var row = val.properties._idHighway + ',POINT(' + val.geometry.coordinates.join(' ') + ')';
+          console.log(row);
+        }
+        coordinates[val.geometry.coordinates.join(',')] = val.geometry.coordinates.join(',');
+      }
+    }
+  }).on('close', function() {
+    done();
+  });
+};
