4281e5e5ba661f2fdea033c2dea87d3f92dc14bd
Rub21 <ruben@mapbox.com>
2016-06-06 15:08:50 -0500

    add missing oneway

---
+++ converters/missingOneway/Readme.md
@@ -0,0 +1,6 @@
+## Execute 
+
+```
+osmlint2csv --conv=missingoneway --type=major missingoneway.tofix.json > missingoneway.csv
+
+```
---
+++ converters/missingOneway/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs = require('fs');
+var _ = require('underscore');
+var readline = require('readline');
+
+module.exports = function(inputFile, type, done) {
+  var types = type.split(',');
+  var rd = readline.createInterface({
+    input: fs.createReadStream(inputFile),
+    output: process.stdout,
+    terminal: false
+  });
+  var header = 'way,geom';
+  //Print CSV header
+  console.log(header);
+  rd.on('line', function(line) {
+    var obj = JSON.parse(line);
+    var result = {};
+    var features = obj.features;
+    for (var i = 0; i < features.length; i++) {
+      var val = features[i];
+      if (val.geometry.type === 'LineString' && types.indexOf(val.properties._type) > -1) {
+        var id = val.properties['@id'];
+        var coor = val.geometry.coordinates;
+        var row = id + ',"MULTIPOINT(' + coor[0].join(' ') + ',' + coor[coor.length - 1].join(' ') + ')"';
+        console.log(row);
+      }
+    }
+  }).on('close', function() {
+    done();
+  });
+};
